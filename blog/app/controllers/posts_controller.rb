f8cc87284b89fb9a6e356f51575de3c50ad6d7f4
MikeyMoore <icarusjedi@gmail.com>
2016-12-11 23:37:02 -0800

    add links

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -1,17 +1,25 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+
   def show
     @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(post_params)
 
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   private
